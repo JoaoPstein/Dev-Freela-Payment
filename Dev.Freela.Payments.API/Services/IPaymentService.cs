7da16ed36060f736cdb82aba736ff8a6d93c9502
Joao Paulo Stein de Campos <joao.campos@ambevtech.com.br>
2022-08-12 07:16:47 -0300

    feat: initial

---
+++ Dev.Freela.Payments.API/Services/IPaymentService.cs
@@ -0,0 +1,9 @@
+﻿using Dev.Freela.Payments.API.Models;
+
+namespace Dev.Freela.Payments.API.Services
+{
+    public interface IPaymentService
+    {
+        Task<bool> Process(PaymentInfoInputModel paymentInfoInputModel);
+    }
+}
---
+++ Dev.Freela.Payments.API/Services/PaymentService.cs
@@ -0,0 +1,16 @@
+﻿using Dev.Freela.Payments.API.Models;
+
+namespace Dev.Freela.Payments.API.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public PaymentService()
+        {
+
+        }
+        public Task<bool> Process(PaymentInfoInputModel paymentInfoInputModel)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
